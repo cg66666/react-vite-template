c112b6a75415e1c00afa35a81e195051e6e548c0
ZhuChenguang <915373240@qq.com>
2023-10-08 14:06:59 +0800

    add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+/*
+ * @Description: file content
+ * @Author: 朱晨光
+ * @Date: 2023-10-08 13:56:55
+ * @LastEditors: 朱晨光
+ * @LastEditTime: 2023-10-08 14:00:32
+ */
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  semi: false
+}
